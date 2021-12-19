52e81318e5b21ce4b0d22548f233952c3c008854
trunglnt <trunglnt@digi-texx.local>
2021-12-19 16:42:34 +0700

    'change-version-router'

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import './Sass/main.css';
 import Header from './Component/Header/header';
 import HomePage from './Pages/HomePage/homePage';
 import Footer from './Component/Footer/footer';
-import { Routes, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomeTemplates from './Templates/HomeTemplates';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/trangchu" element={<HomePage />} />
-      </Routes>
-      <Footer />
+      <Switch>
+        <HomeTemplates exact path="/" Component={HomePage} />
+        <HomeTemplates exact path="/trangchu" Component={HomePage} />
+      </Switch>
     </div>
   );
 }
